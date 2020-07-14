2146107f25a436b46afe2aad5670b7d7fe7f1166
Raymond Doran <raymond@thedanielfactor.com>
2020-07-14 12:04:16 -0500

    Updates to my code

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,12 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+## EASY ADD ALIASES
+## ex. `add-alias spv "spotify vol"` will be `alias spv="spotify vol"` in zsh file
+add-alias() {
+  # you can specify a 3rd arg as new location in ~/.dotfiles if you so desire
+  DEFAULT=$HOME/.dotfiles/aliases/aliases.symlink
+  echo "alias $1=\"$2\"" >>${3:-$DEFAULT}
+  source ~/.zshrc
+}
